eb459b55943a349cd14bdf03e600fb04cecb1eb1
barcarlos02 <cabrizu@gmail.com>
2019-06-22 20:20:31 -0500

    login

--- FrontEnd/login.php
+++ FrontEnd/login.php
@@ -1,27 +1,23 @@
 <?php
+session_start();
 include("services/apifunctions.php");
 include("services/resources.php"); //Export api URL
 $correo=$_POST['correo'];
 $contrasena=$_POST['contrasena'];
 $url = $apiurl . "login/"; //concat the api url with the uri of the service
-/*$data = array(
-  'correo' => 'cabrizi@gmail.com',
-  'nombre' => 'Carlos Briano',
-  'edad' => '23',
-  'telefono' => '4492555531',
-  'sexo' => 'M',
-  'direccion' => 'Jesus Consuelo 1505',
-  'fecha_nacimiento' => '1995/12/15',
-  'estado' => 'Aguascalientes',
-  'contrasena' => '123'
-);*/
 $url=$url . $correo . "&" . $contrasena;
 $res=getDataapi($url);
-echo json_encode($res);
+$array= json_encode($res);
 if(count($res)==0){
   echo "Nombre de usuario o contraseña incorrectos, intentalo de nuevo";
 }  
 else{
+  $_SESSION['nombre'] = $array[0]["nombre"];
+  $_SESSION['correo'] = $array[0]["correo"];
+  $_SESSION['estado'] = $array[0]["estado"];
+  $_SESSION['tipo'] = $array[0]["tipo"];
+  $_SESSION['direccion'] = $array[0]["direccion"];
+  $_SESSION['telefono'] = $array[0]["telefono"];
   header("location:registrarse.html");
 }  
 ?>
